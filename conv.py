da9f687eb76a2b489bd516f29c54e9e3b4405812
w <wykchina@gmail.com>
2017-12-03 12:31:23 +0800

    转换功能改为jl

--- conv.py
+++ conv.py
@@ -2,14 +2,20 @@ import json
 
 import xlwt
 
-data = json.load(open('data.json', 'r'))
+# data = json.load(open('data.jl', 'r'))
 
 work_book = xlwt.Workbook()
 sheet = work_book.add_sheet('data')
-
-for index, line in enumerate(data):
+index = 0
+for line in open('scraped_data.jl').readlines():
+    try:
+        line = json.loads(line)
+    except:
+        break
+    # for index, line in enumerate(data):
     # print line['title'][:-1], '\t', line['news_type'][:-1], '\t',line['content']
     sheet.write(index, 0, line['title'])
     sheet.write(index, 1, line['news_type'])
-    sheet.write(index, 2, line['content'])
+    sheet.write(index, 2, line['content'].strip())
+    index += 1
 work_book.save('data.xls')
